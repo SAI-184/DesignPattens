09edd264e048d5a9a5e00de4be13ff8855a9de81
Swarna Sai Viswanth (DevOn) <s.viswanth@devon.nl>
2025-01-24 23:10:08 +0530

    factory pattern

---
+++ Factory/PaymentTypeFactory.cs
@@ -0,0 +1,26 @@
+﻿using Factory.Classes;
+using Factory.Inferface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Factory
+{
+    public class PaymentTypeFactory
+    {
+        public IPaymentType Create(string paymentType) 
+        {
+            if (paymentType.Equals("1"))
+            {
+                return new CreditCardPaymentType();
+            }
+            else if (paymentType.Equals("2"))
+            {
+                return new DebitCardPaymentType();
+            }
+            return new CreditCardPaymentType();
+        }
+    }
+}

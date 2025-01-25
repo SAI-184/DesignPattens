e38a6890a61d276cdb195e402567dfac0b61caf9
VISWANTH <SAIV436777@exactsoftware.com>
2025-01-25 20:35:21 +0530

    decorator design pattern

---
+++ DecoratorPattern/Classes/BaseNotificationDecorator.cs
@@ -0,0 +1,22 @@
+﻿using DecoratorPattern.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DecoratorPattern.Classes
+{
+    public class BaseNotificationDecorator : INotifier
+    {
+        private readonly INotifier notifier;
+        public BaseNotificationDecorator(INotifier notifier)
+        {
+            this.notifier = notifier;
+        }
+        public void Notify()
+        {
+            notifier.Notify();
+        }
+    }
+}

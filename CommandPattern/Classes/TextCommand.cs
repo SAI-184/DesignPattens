ec30ae96b60091adfbb7d52bb52438e4ce1f18ac
VISWANTH <SAIV436777@exactsoftware.com>
2025-01-27 21:49:02 +0530

    Template design pattern

---
+++ CommandPattern/Classes/TextCommand.cs
@@ -0,0 +1,17 @@
+﻿using CommandPattern.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommandPattern.Classes
+{
+    public class TextCommand : ICommand
+    {
+        public void execute()
+        {
+            Con
+        }
+    }
+}
